b5f5ba4eb2cd67315c4b9447a6bc13e7b4205b6d
Sverrir Sigmundarson <sverrirs@gmail.com>
2018-01-11 21:45:26 +0100

    Bumping version to 2.1 prior to release

--- vsgallery/Properties/AssemblyInfo.cs
+++ vsgallery/Properties/AssemblyInfo.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("vsgallery")]
 [assembly: AssemblyProduct("vsgallery")]
-[assembly: AssemblyCopyright("Copyright Sverrir Sigmundarson © 2017")]
+[assembly: AssemblyCopyright("Copyright Sverrir Sigmundarson © 2017-2018")]
 [assembly: ComVisible(false)]
 [assembly: Guid("569af97a-4957-421e-8747-fbc5ca09b0e9")]
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.1.*")]
